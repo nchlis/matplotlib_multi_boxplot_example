19353fc05ef9429a1c63d07f9dc519de5185216c
nchlis <nchlis@isc.tuc.gr>
2019-06-18 14:35:26 +0200

    wrote README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# matplotlib_multi_boxplot_example
-How to plot multiple boxplots in the same figure. Useful when wanting to visualize e.g. model uncertainty
+
+# Visualizing model performance with boxplots
+How to plot multiple boxplots in the same figure. Useful when we need to visualize the mean/median model performance, along with variability (standard deviation/interquantile range).
+Let us assume we have a number of trained models and we want to assess how they perform
+on the same intependent test set using a performance metric, such as accuracy, Dice, AUC, etc.
+
+## Independent models
+When evaluating independent models it is useful to plot their performance as a 
+boxplot. Boxplots are a convenient way to visualize the variability of a model's
+performance on a set of datapoints (that comprise the test set).
+
+If we prefer a cleaner look, we can only plot the medians, along with error-bars
+that correspond to the 25th and 75th percentile (showing the interquantile range)
+
+The performance of different models can be visualized via boxplots as
+![](model_boxplots.png)
+**Figure 1** Visualizing the performance of independent models via boxplots.
+
+![](model_errorbars.png)
+**Figure 2** Visualizing the performance of independent models via median+error bars corresponding to the interquantile range (the 25th and 75th percentiles).
+
+## Dependent models
+Sometimes we want to visualize a series of dependent models. E.g. how the performance
+of a model changes after training for additional epochs, or after adding new datapoints
+to the training set. In this case, it is convenient to visualize this continuity
+by connecting the boxplots. In this example, we connect the boxplots by drawing a 
+line that connects the medians
+
+Similarly, we can connect the medians of the cleaner plot that only visualizes
+the median and the interquantile range.
+
+![](model_boxplots_connected.png)
+**Figure 3** Visualizing the performance of dependent models via boxplots.
+
+![](model_errorbars_connected.png)
+**Figure 4** Visualizing the performance of dependent models via median+error bars corresponding to the interquantile range (the 25th and 75th percentiles).
+
+The plots are generated using artificial data. All plots are created in generate_plot.py.
+
+## About
+Personal website: https://users.isc.tuc.gr/~nchlis/
+
+For tutorials on Machine Learning projects you can visit
+- https://nchlis.github.io/
+- https://github.com/nchlis/
+
---
+++ generate_plot.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 11:26:25 2019
+
+@author: N.Chlis
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+connected = True
+#connected = False
+
+#%%
+
+Ndatapoints = 10 #number of datapoints in the test set
+Nmodels = 5 #number of models trained
+
+#matrix containing the data
+#rows: test set datapoints
+#columns: models
+#each elements corresponds to the score (e.g. accuracy) of a model on a dataset
+X = np.zeros((Ndatapoints,Nmodels),dtype='float')#initialize to zeros
+
+np.random.seed(1)#set random seed for repeatability
+
+#now generate artificial data, for the sake of plotting
+for m in range(Ndatapoints):
+    for d in range(Nmodels):
+        X[m,d] = np.random.uniform(low=0,high=1)
+
+#%% generate a plot of boxplots whose medians are connected by a line
+        
+figure_size = 4
+fig, ax = plt.subplots(figsize=(figure_size*Nmodels,figure_size))
+box_data = X
+md = np.median(box_data,axis=0)#median
+
+#ax.boxplot plots each column as a separate boxplot
+bplots = ax.boxplot(box_data)
+
+xticks = np.arange(Nmodels)+1
+
+if connected == True:
+    #make the boxplots transparent
+    for key in bplots.keys():
+        #print(key)
+        for b in bplots[key]:
+            b.set_alpha(0.2)
+    #add a line that connects the medians of all boxplots
+    ax.plot(xticks,md,marker='o',c='black',lw=5,markersize=15,label='median')
+
+ax.set_ylabel('Score (test set)',{'fontsize':16})
+ax.set_xlabel('Model',{'fontsize':16})
+ax.set_title('Model Performance',{'fontsize':16})
+ax.set_ylim(0,1)
+
+#generate the xtick labels
+xtick_labels = []
+for m in xticks:
+    xtick_labels.append('Model '+str(m))
+ax.set_xticklabels(xtick_labels,rotation = 30, ha='center',fontsize=10)
+
+#save the figure to disk
+if connected == True:
+    plt.savefig('model_boxplots_connected.png',dpi=200,bbox_inches='tight')
+else:
+    plt.savefig('model_boxplots.png',dpi=200,bbox_inches='tight')
+
+#%% redo the same plot without the boxplots
+# only plot a line for the medians, along with error-bars for interquantile range
+
+figure_size = 4
+fig, ax = plt.subplots(figsize=(figure_size*Nmodels,figure_size))
+box_data = X
+xticks = np.arange(Nmodels)+1
+
+md = np.median(box_data,axis=0)
+yerr = np.zeros((2,Nmodels))
+for m in range(Nmodels):
+    #plt.errorbar needs the difference between the percentile and the median
+    #lower errorbar: 25th percentile
+    yerr[0,m]=md[m]-np.percentile(X[:,m],25)#lower errorbar
+    #upper errorbar: 75th percentile
+    yerr[1,m]=np.percentile(X[:,m],75)-md[m]
+
+#plot the errorbars
+ax.errorbar(xticks,md,yerr,capsize=10,fmt='none',c='black')
+#fmt='none' to only plot the errorbars
+
+#plot the (optional) connecting line
+if connected == True:
+    ax.plot(xticks,md,marker='o',c='black',lw=5,markersize=15,label='median')
+else:
+    ax.scatter(xticks,md,marker='o',c='black',s=200)
+
+ax.set_ylabel('Score (test set)',{'fontsize':16})
+ax.set_xlabel('Model',{'fontsize':16})
+ax.set_title('Model Performance',{'fontsize':16})
+ax.set_ylim(0,1)
+
+ax.set_xticks(xticks)
+#generate the xtick labels
+xtick_labels = []
+for m in xticks:
+    xtick_labels.append('Model '+str(m))
+ax.set_xticklabels(xtick_labels,rotation = 30, ha='center',fontsize=10)
+
+if connected == True:
+    plt.savefig('model_errorbars_connected.png',dpi=200,bbox_inches='tight')
+else:
+    plt.savefig('model_errorbars.png',dpi=200,bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
